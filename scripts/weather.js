26f69b29356832b7293b99b0fb4e3800718b45b2
Oluwaseun <adunmofestus@gmail.com>
2023-09-28 01:00:02 +0100

    querySelector added

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,27 +1,24 @@
-const apiKey = "863242cfb2b1d357e6093d9a4df19a4b";
+const apiKey = "dafc64499575e06a52877327de807cd4";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-const searchBox = document.querySelector(".search input")
-const searchBtn = document.querySelector(".search button")
 
-async function checkweather(city){
-    const response = await fetch(`${apiUrl} + {city} + &appid=${apiKey}`);
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
-
-    console.log(response)
 
     console.log(data);
 
-    document.querySelector(".city").innerHTML = data;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)  + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
 }
-  
 
 searchBtn.addEventListener("click", ()=>{
-    checkweather(searchBox.value);
+    checkWeather(searchBox.value);
 })
-
-
